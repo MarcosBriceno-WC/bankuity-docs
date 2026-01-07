ef5a6001ab4a1930e060bc3f7883540a4cc4e497
Marcos Briceno <155094145+MarcosBriceno-WC@users.noreply.github.com>
2026-01-07 17:29:39 -0300

    Final Push of POC

---
+++ snippets/homebg.jsx
@@ -0,0 +1,13 @@
+export const HomeBg = () => {
+  return (
+    <>
+      <style>
+        {`
+        #almond-background-color {
+          background-color: #11132D;
+        }
+        `}
+      </style>
+    </>
+  );
+};
